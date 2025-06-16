3e97b5cf9f4c5ed90713518f1bd9802a9a0bec79
paulhan97 <paul.seungoh.han@gmail.com>
2025-06-16 04:56:16 +0000

    Augment key handling for characters with double inputs

--- widgets/greek_text_input.py
+++ widgets/greek_text_input.py
@@ -8,22 +8,47 @@ class GreekTextInput(Input):
 
 
     character_mapping = {
-        'a': 'α'
+        'a': 'α',
+        'b': 'β',
+        'g': 'γ',
+        'd': 'δ',
+        'e': 'ε',
+        'z': 'ζ',
+        'i': 'ι',
+        't': 'τ'
     }
 
 
     def on_key(self, event: events.Key) -> None:
         self._restart_blink()
+        event.stop()
+        character = event.character
+        character_before_insert_position = self.value[:(
+            self.cursor_position
+            if self.selection.is_empty
+            else self.selection.start
+        )][-1:]
+        replace_character_before_insert_position = False
 
-        if (
-            event.is_printable
-            and event.character in self.character_mapping
-        ):
-            event.stop()
-            selection = self.selection
-            character = self.character_mapping[event.character]
-            if selection.is_empty:
-                self.insert_text_at_cursor(character)
+        if character in self.character_mapping:
+            character = self.character_mapping[character]
+        elif character == 'h':
+            match character_before_insert_position:
+                case 'τ':
+                    character = 'θ'
+                    replace_character_before_insert_position = True
+
+        selection = self.selection
+        if selection.is_empty:
+            if replace_character_before_insert_position:
+                self.replace(character, self.cursor_position - 1, self.cursor_position)
             else:
-                self.replace(character, *selection)
-            event.prevent_default()
+                self.insert_text_at_cursor(character)
+        else:
+            start = (
+                selection.start - 1
+                if replace_character_before_insert_position
+                else selection.start
+            )
+            self.replace(character, start, selection.end)
+        event.prevent_default()
